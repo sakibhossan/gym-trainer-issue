bb61c5905b1f24ff8ffc9ce26fb65be7831380c8
sakib213 <sakibhss438@gmail.com>
2022-07-11 21:26:43 +0600

    blogs components setup

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Login from './UniquePages/Login/Login/Login';
 import Register from './UniquePages/Login/Register/Register';
 import CheckOut from './UniquePages/CheckOut/CheckOut/CheckOut';
 import RequireAuth from './UniquePages/Login/RequireAuth/RequireAuth';
+import Blogs from './UniquePages/Blogs/Blogs';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
     <Route path='/home' element={<Home></Home>}></Route>
     <Route path='/workout/:workoutId' element={<WorkOutDetail></WorkOutDetail>}></Route>
     <Route path='/about' element={<About></About>}></Route>
+    <Route path='/blogs' element={<Blogs></Blogs>}></Route>
     <Route path='/login' element={<Login></Login>}></Route>
     <Route path='/register' element={<Register></Register>}></Route>
     <Route path='/checkout' element={
---
+++ src/UniquePages/Blogs/Blogs.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>1 Difference between authorization and authentication?</h2>
+            <p>Ans: Simply put, authentication is the process of verifying who someone is, whereas authorization is the process of verifying what specific applications, files, and data a user has access to. The situation is like that of an airline that needs to determine which people can come on board</p>
+            <br />
+            <h2>2. Why are you using firebase? What other options do you have to implement authentication?</h2>
+
+            <p>Ans: Firebase Authentication aims to make building secure authentication systems easy, while improving the sign-in and onboarding experience for end users. It provides an end-to-end identity solution, supporting email and password accounts, phone auth, and Google, Twitter, Facebook, and GitHub login, and more.</p>
+            <br />
+            <h2>3.What other services does firebase provide other than authentication?</h2>
+            
+            <p>Ans: Cloud Firestore,Cloud Functions,Authentication,Hosting,Cloud Storage,Google Analytics,Predictions,Cloud Messaging,Dynamic Links,
+            Remote Config
+</p>
+        </div>
+    );
+};
+
+export default Blogs;
